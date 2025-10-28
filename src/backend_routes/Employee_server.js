040990394aa3269f16cad2f6cae8168b43273809
pavithran <pavithran@LAPTOP-LA9M0L2P>
2025-10-28 13:56:32 +0530

    added is active condition

--- src/backend_routes/Employee_server.js
+++ src/backend_routes/Employee_server.js
@@ -10,7 +10,7 @@ router.get("/all", (req, res) => {
   const query = `
     SELECT * 
     FROM employee
-    WHERE deleted_by IS NULL 
+    WHERE deleted_by IS NULL AND is_active = '1'
     ORDER BY created_time DESC 
   `;
 
